0a31f5e8ec2e6bc9ad6ead5c9c0d8a8106a39dc6
andreakollova <andrea_kollova@hotmail.com>
2026-07-08 11:37:41 +0200

    NameModal: Hi Im Coduy the small cute little code guy

--- src/data/strings.ts
+++ src/data/strings.ts
@@ -107,8 +107,8 @@ const strings = {
   nextLesson: { en: 'Next Lesson', sk: 'Ďalšia lekcia' },
 
   // NameModal
-  heyImByte: { en: "Hey, I'm Byte", sk: 'Ahoj, ja som Byte' },
-  illTeachYou: { en: "I'll teach you how to code. But first — what's your name?", sk: 'Naučím ťa programovať. Ale najprv — ako sa voláš?' },
+  heyImByte: { en: "Hi, I'm Coduy", sk: 'Ahoj, ja som Coduy' },
+  illTeachYou: { en: "I'm the small cute little code guy. I'll teach you how to code. But first — what's your name?", sk: 'Som malý roztomilý kóderský chlapík. Naučím ťa programovať. Ale najprv — ako sa voláš?' },
   yourName: { en: 'Your name...', sk: 'Tvoje meno...' },
   letsGo: { en: "Let's go", sk: 'Poďme' },
 
